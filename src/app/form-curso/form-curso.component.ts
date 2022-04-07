6f063064317f6e512e8470c21b94e3f73610e626
Walter Peña <walter0614_@hotmail.com>
2022-04-07 16:06:49 -0500

    Update form-curso.component.ts

--- src/app/form-curso/form-curso.component.ts
+++ src/app/form-curso/form-curso.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { ApiServiceService } from '../services/api-service.service';
 
 @Component({
   selector: 'app-form-curso',
@@ -12,9 +13,40 @@ export class FormCursoComponent implements OnInit {
   fecha_inicio_cursoCurso = new FormControl;
   fecha_final_cursoCurso = new FormControl;
 
-  constructor() { }
+  constructor(private apiService:ApiServiceService) { }
 
   ngOnInit(): void {
+  }
+
+  save(){
+    
+    // this.apiService.Post("cursos",
+    // {
+    //   id:6,
+    //   nombre:this.nombreCurso.value(),
+    //   fecha_inicio_curso:"asdas",
+    //   fecha_final_curso:"asdsa"
+    // }
+    // ).then(x => {
+    //   alert("El curso se creo")
+    // });
+
+    // this.apiService.Update("cursos",
+    // {
+    //   id:6,
+    //   nombre:this.nombreCurso.value(),
+    //   fecha_inicio_curso:"asdas",
+    //   fecha_final_curso:"asdsa"
+    // },
+    // "1"
+    // ).then(x => {
+    //   alert("El curso se actualizo")
+    // });
+
+    // this.apiService.Delete("cursos","1")
+    // .then(x => {
+    //   alert("El curso se actualizo")
+    // });
   }
 
 }
